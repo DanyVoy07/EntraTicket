979d94854635bd976eab1ea913a6a16576ee638f
Juan Lustres <lusjuanmanu@gmail.com>
2024-09-26 17:07:15 -0300

    Agregar archivos de proyecto.

---
+++ EntraTicket/Program.cs
@@ -0,0 +1,66 @@
+using EntraTicket.Data;
+using EntraTicket.Repositories; 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using EntraTicket; 
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Agrega servicios al contenedor.
+builder.Services.AddControllers();
+
+// Configura la cadena de conexión
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Registra los repositorios en el contenedor de servicios
+builder.Services.AddScoped<EventRepository>(sp => new EventRepository(connectionString));
+builder.Services.AddScoped<Metodos>(); // Registrar la clase Metodos
+
+// Configuración para JWT
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+
+// Configuración para Swagger (Documentación de API)
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configuración de middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Configura el middleware para archivos estáticos
+app.UseStaticFiles(); // Permite servir archivos estáticos desde wwwroot
+
+app.UseHttpsRedirection();
+
+// Configuración de autenticación y autorización
+app.UseAuthentication(); 
+app.UseAuthorization();
+
+// Configura la página de inicio
+app.MapGet("/", () => Results.File("wwwroot/index.html"));
+
+app.MapControllers();
+
+app.Run();
